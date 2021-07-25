4f779dc5acfdbf58884fc3f7ae8dfb0f0356e0d8
thiago-porto25 <thiago_porto25@hotmail.com>
2021-07-25 09:09:09 -0300

    create hover for the board

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Square() {
-  return <div className="board-square"></div>
+export default function Square({ onClick = false }) {
+  const [isMouseIn, setIsMouseIn] = useState(false)
+
+  return (
+    <div
+      className="board-square"
+      style={{ backgroundColor: isMouseIn ? 'rgb(44, 44, 44)' : 'white' }}
+      onMouseEnter={() => setIsMouseIn(true)}
+      onMouseLeave={() => setIsMouseIn(false)}
+      onClick={onClick}
+    ></div>
+  )
 }
