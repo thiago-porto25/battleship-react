d638d4143b9d601ca5cf64a3c6b0357a18f8ebe0
thiago-porto25 <thiago_porto25@hotmail.com>
2021-07-25 09:36:42 -0300

    change prep square hover styling

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,10 +3,15 @@ import React, { useState } from 'react'
 export default function Square({ onClick = false }) {
   const [isMouseIn, setIsMouseIn] = useState(false)
 
+  const styles = {
+    backgroundColor: isMouseIn ? 'rgb(220, 220, 220)' : 'white',
+    border: isMouseIn ? '1px solid gray' : '1px solid black',
+  }
+
   return (
     <div
       className="board-square"
-      style={{ backgroundColor: isMouseIn ? 'rgb(44, 44, 44)' : 'white' }}
+      style={styles}
       onMouseEnter={() => setIsMouseIn(true)}
       onMouseLeave={() => setIsMouseIn(false)}
       onClick={onClick}
